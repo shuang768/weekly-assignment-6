fb53751de70aad25e637d8377521ba9cd4b14386
shuang768 <113189677+shuang768@users.noreply.github.com>
2022-12-08 15:48:59 +0000

    done

---
+++ main.py
@@ -0,0 +1,29 @@
+pi=0
+value=-1
+def calculate(c,v,d):
+  for i in range(0,c):
+    if i%2==0:
+      v=v+2
+      d=d-4/v
+      print("pi with the iteration",i+1,"is\n")
+      print(abs(d),"\n\n")
+    else:
+      v=v+2
+      d=d+4/v
+      print("pi with the iteration",i,"is\n")
+      print(abs(d),"\n\n")
+task=0
+while True:
+  try:
+    task=int(input("enter your iteration for π\n"))
+  except ValueError:
+      print("Please enter a number")
+      continue
+      
+  else:
+    if task<1:
+        continue
+    else: 
+        calculate(task,value,pi)
+        break
+      
